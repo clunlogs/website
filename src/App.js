db4f311f1b3f724ed6ffcfa31669fa91f3ea0551
Andrew Morgan <theclunlogcompanywebsite@gmail.com>
2024-08-18 20:16:15 +0100

    Start

---
+++ src/App.js
@@ -0,0 +1,15 @@
+//import logo from './logo.svg';
+import logo_main from './log_logo.jpg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo_main} width="20%" alt="Main Logo"/>
+      </header>
+    </div>
+  );
+}
+
+export default App;
